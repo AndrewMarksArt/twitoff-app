c4d5d8da4f9523370690ca651595eaf055d43a1e
AndrewMarksArt <andrew.colin.marks@gmail.com>
2019-12-11 07:11:49 -0800

    added form to search for twitter users, grab the 200 most recent tweets excluding replies and retweets, store them in the DB, query the DB in our web app, loop through and display tweets from the query

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,22 +1,47 @@
 import uuid
-from flask import Flask, render_template
+from decouple import config
+from flask import Flask, render_template, request
 from .models import DB, User, Tweet
-
+from .twitter import add_or_update_user
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_db.sqlite'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['ENV'] = config('ENV')
     DB.init_app(app)
 
     @app.route('/')
-    def index():
-        rand_name = str(uuid.uuid4())
-        rand_u = User(name=rand_name)
-        DB.session.add(rand_u)
-        DB.session.commit()
-        return 'Index Page'
+    def root():
+        DB.create_all()
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
+
 
     @app.route('/hello')
     def hello():
         return render_template('base.html', title='hello')
+    
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='DB Reset!', users=[])
+
+    
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = f'User {name} successfully added!'
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = f'Error adding {name}: {e}'
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets, message=message)
+
     return app
